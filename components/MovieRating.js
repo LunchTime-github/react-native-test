e36e2d9c4b1f2e0589b36e5545f2b06a106a0651
LunchTime-github <hiyoon9@gmail.com>
2020-02-13 15:42:55 +0900

    # 5 Sliders in React Native

---
+++ components/MovieRating.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { TINT_COLOR, GREY_COLOR } from "../constants/Colors";
+
+const Vote = styled.Text`
+  color: ${props => props.inSlide ? TINT_COLOR : GREY_COLOR};
+  font-size: ${props => props.inSlide ? "10px" : "8px"};
+`;
+
+const MovieRating = ({ votes, inSlide = false }) => <Vote>⭐{`${votes} / 10`}</Vote>;
+
+MovieRating.propTypes = {
+  votes: PropTypes.number.isRequired,
+  inSlide: PropTypes.bool
+};
+
+export default MovieRating;
--- screens/Movies/MoviesPresenter.js
+++ screens/Movies/MoviesPresenter.js
@@ -2,9 +2,24 @@ import React from "react";
 import { Text } from "react-native";
 import PropTypes from "prop-types";
 import Loader from "../../components/Loader";
+import styled from "styled-components";
+import MoiveSlider from "../../components/MovieSlider";
+import { BG_COLOR } from "../../constants/Colors";
+
+const Container = styled.ScrollView`
+  background-color: ${BG_COLOR};
+  flex: 1;
+`;
 
 const MoviesPresenter = ({ loading, upcoming, popular, nowplaying }) =>
-  loading ? <Loader /> : <Text>Movies</Text>;
+  loading ? (
+    <Loader />
+  ) : (
+    <Container>
+      <MoiveSlider movies={nowplaying} />
+      <Text>Movies</Text>
+    </Container>
+  );
 
 MoviesPresenter.propTypes = {
   loading: PropTypes.bool.isRequired,
